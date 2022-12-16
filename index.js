b07396e4e39b224f9a167afc2ef077e6c4fccf4f
alfons22 <vc70383@hotmail.com>
2022-12-15 21:26:14 -0600

    Endpoint 1 Created
    - Endpoint to return the data from https://randomuser.me/api?

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-const http = require('http');
-
-
-
 const express = require('express');
 const app = express();
-const fetch = require('node-fetch');
 
 app.get('/random-users', (req, res) => {
     fetch('https://randomuser.me/api?results=10')
         .then(response => response.json())
-        .then(data => res.send(data));
+        .then(data => {
+            res.send(data);
+        });
 });
 
-app.listen(3000, () => console.log('Server listening on port 3000'));
+app.listen(3000, () => {
+    console.log('Server listening on port 3000');
+});
