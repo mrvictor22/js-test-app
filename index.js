bce97c0f9f7823afbeb6f1b4ddbf894bcf148c04
alfons22 <vc70383@hotmail.com>
2022-12-15 22:32:02 -0600

    Endpoint 2 - Added base code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-
+const bodyParser = require('body-parser');
 app.get('/random-users', (req, res) => {
     fetch('https://randomuser.me/api?results=10')
         .then(response => response.json())
@@ -9,6 +9,54 @@ app.get('/random-users', (req, res) => {
         });
 });
 
+
+
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+let users = [];
+
+// POST /users
+app.post('/users', (req, res) => {
+    // create a new user from the request body
+    const user = req.body;
+
+    // save the user in the database or in-memory store
+    users.push(user);
+
+    // return the saved user
+    res.json(user);
+});
+
+// GET /users
+app.get('/users', (req, res) => {
+    // fetch saved users and mix them with random users
+    const mixedUsers = [...users, ...fetchRandomUsers()];
+
+    // return the mixed users
+    res.json(mixedUsers);
+});
+
+// DELETE /users
+app.delete('/users/:id', (req, res) => {
+    // get the id of the user to delete from the request parameters
+    const id = req.params.id;
+
+    // find the index of the user in the users array
+    const index = users.findIndex(user => user.id === id);
+
+    // remove the user from the array
+    users.splice(index, 1);
+
+    // return a message indicating that the user was deleted
+    res.send(`User with id ${id} deleted`);
+});
+
+// start the server
 app.listen(3000, () => {
     console.log('Server listening on port 3000');
 });
