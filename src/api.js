2bcaf5006e0acffa2e7cc28b8aea28ce838b615b
alfons22 <vc70383@hotmail.com>
2022-12-19 00:05:30 -0600

    test 14
    test 15
    
    
    test 16
    
    
    test 17
    
    
    test 18
    
    
    added delete endpoint

--- src/api.js
+++ src/api.js
@@ -3,14 +3,15 @@ const { faker } = require('@faker-js/faker');
 const bodyParser = require('body-parser');
 const serverless = require("serverless-http");
 const app = express();
-const router = express.Router();
+const router = express.Router()
+const cors = require("cors");;
 const axios = require('axios');
 router.get("/", (req, res) => {
     res.json({
         hello: "hi!"
     });
 });
-
+app.use(cors())
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -18,21 +19,76 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 let users = [];
+let ramusers;
+
+axios.get('https://randomuser.me/api?results=10')
+    .then(function (response) {
+        ramusers = response.data.results;
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+
+function getUsers() {
+    return ramusers;
+}
 
 router.get('/random-users', (req, res) => {
-
-
-    axios.get('https://randomuser.me/api?results=10')
-        .then(function (response) {
-            res.send(response.data.results);
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
-
-
+    res.send(getUsers());
 });
 
+// POST /users
+router.post('/users', (req, res) => {
+    // create a new user from the request body
+    const user = req.body;
+
+    // save the user in the database or in-memory store
+    users.push(user);
+
+    // return the saved user
+    res.json(user);
+});
+
+
+// GET /users
+router.get('/users', (req, res) => {
+    // fetch saved users and mix them with random users
+    const mixedUsers = [...users, ...fetchRandomUsers()];
+
+    // return the mixed users
+    res.json({user: mixedUsers});
+});
+
+// DELETE /users
+router.delete('/users/:id', (req, res) => {
+    // get the id of the user to delete from the request parameters
+    const id = req.params.id;
+
+    // find the index of the user in the users array
+    const index = users.findIndex(user => user.id === id);
+
+    // remove the user from the array
+    users.splice(index, 1);
+
+    // return a message indicating that the user was deleted
+    res.send(`User with id ${id} deleted`);
+});
+
+
+function fetchRandomUsers() {
+    const numUsers = Math.floor(Math.random() * 10) + 1;
+    const users = [];
+    for (let i = 0; i < numUsers; i++) {
+        const user = {
+            id: faker.datatype.uuid(),
+            name: faker.internet.userName(),
+            email: faker.internet.email(),
+            avatar: faker.image.avatar()
+        };
+        users.push(user);
+    }
+    return users;
+}
 
 app.use(`/.netlify/functions/api`, router);
 
